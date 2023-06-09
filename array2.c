e61da601467465e595b0c38d70121a39ccf4dca1
Jaybhadiyadra22 <131762319+Jaybhadiyadra22@users.noreply.github.com>
2023-06-09 14:46:37 +0530

    Add files via upload

---
+++ array2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>    
+     
+int main()    
+{    
+    int arr[] = {1, 2, 3, 4, 5};     
+           
+    int length = sizeof(arr)/sizeof(arr[0]);    
+    printf("Number of elements array: %d", length);     
+     
+}    
